399704bb76bc0904dd39b3ae6e57a61d616e4d7e
ATANU MANDAL <atanu2697@gmail.com>
2026-03-27 20:38:50 +0530

    Update canWinNim.java

--- canWinNim.java
+++ canWinNim.java
@@ -1,6 +1,10 @@
 //292
 class Solution {
     public boolean canWinNim(int n) {
-        return (n%4 != 0);
+        if (n%4 != 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
